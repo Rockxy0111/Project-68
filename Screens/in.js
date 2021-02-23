baca21a8d02654288122fbf36348ae53e3c73295
RanveerKhokhar <72133434+Rockxy0111@users.noreply.github.com>
2021-02-23 15:03:35 +0530

    Add files via upload

---
+++ Screens/in.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+
+export default class SearchScreen extends React.Component {
+    render(){
+    return (
+      <View >
+      
+
+        <Text style={styles.container}>Instagram</Text>
+      </View>
+    );
+    }
+  }
+
+
+  const styles = StyleSheet.create({
+    container: {
+    
+
+       marginLeft:'50%',
+    fontSize:22,
+      marginTop:300,
+    },
+  });
